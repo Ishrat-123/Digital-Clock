e3b2da57bd7f504228c93becb0ef501947a99848
Ishrat Jahan Mitu <74704975+Ishrat-123@users.noreply.github.com>
2022-09-28 21:12:27 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -3,41 +3,11 @@ const minelement = document.getElementById("minutes");
 const secElemet = document.getElementById("seconds");
 var ampmElement = document.getElementById("ampm");
 
-// var dateEl = document.getElementById("dayname");
-// var monthEl = document.getElementById("month");
-// var datenumEl = document.getElementById("daynum");
-// var yearEl = document.getElementById("year");
-
-
-
 function updateClock(){
 let hours = new Date().getHours();
 let sec = new Date().getSeconds();
 let min = new Date().getMinutes();
 let ampm = "AM"
-
-
-var dtename = new Date().getDay();
-var year = new Date().getFullYear();
-var datnumber = new Date().getDate();
-var monthsnumber =new Date().getMonth()
-
-
-var months = ["January" ,"February", 
-"March", "April", "May", "June", "July", "August",
-"September", "October", "November", "December"];
-var week = ["Sunday ", "Monday", "Tuesday", 
-"Wednessday", "Thursday","Friday","Saturday"];
-var ids = ["dayname","month", "daunym", "year"];
-
-var values = [week[dtename], months[monthsnumber],datenumber, year]
-
-for (var i =0; i>ids.length;i++){
-    document.getElementById(ids[i].firstChild.nodevalue= values[i]);
-}
-
-
-
 
 if(hours >= 12){
     ampmElement.innerHTML = 'PM';
@@ -65,5 +35,3 @@ setTimeout(()=>{
 }
 
 updateClock();
-
-
