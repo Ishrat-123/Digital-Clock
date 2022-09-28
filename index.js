37bd507eed0992193611eb3c2f8ef2e09b8e3cce
Ishrat Jahan Mitu <74704975+Ishrat-123@users.noreply.github.com>
2022-09-28 22:08:33 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -3,38 +3,11 @@ const minelement = document.getElementById("minutes");
 const secElemet = document.getElementById("seconds");
 var ampmElement = document.getElementById("ampm");
 
-
-var months= ["January" ,"February", 
-"March", "April", "May", "June", "July", "August",
-"September", "October", "November", "December"];
-var weeks = ["Sunday", "Monday", "Tuesday", 
-"Wednesday", "Thursday","Friday","Saturday"];
-
-
-var monthEl = document.getElementById("month");
-var datenumEl = document.getElementById("daynum");
-var yearEl = document.getElementById("year");
-var daynamEl = document.getElementById("dayname");
-
 function updateClock(){
-
-
-let dayToday = new Date().getDay();
-let year = new Date().getFullYear();
-let datnumber = new Date().getDate();
-let monthsnumber =new Date().getMonth()
-
-monthEl.innerHTML = months[monthsnumber];
-datenumEl.innerHTML = datnumber;
-yearEl.innerHTML = year;
-daynamEl.innerHTML = weeks[dayToday];
-
-
 let hours = new Date().getHours();
 let sec = new Date().getSeconds();
 let min = new Date().getMinutes();
 let ampm = "AM"
-
 
 if(hours >= 12){
     ampmElement.innerHTML = 'PM';
@@ -56,13 +29,9 @@ minelement.innerText = min;
 secElemet.innerText = sec;
 ampmElement,(innerText = ampm);
 
-
-
 setTimeout(()=>{
     updateClock()
 }, 1000)
 }
 
 updateClock();
-
-
